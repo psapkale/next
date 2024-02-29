0d0dbbddfbd16f613eabb6a7bb11e1c170ad0c2b
Prem <prempravinsapkale@gmail.com>
2024-02-29 17:37:05 +0530

    title metadata

--- routing-demo/src/app/blog/page.tsx
+++ routing-demo/src/app/blog/page.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+   title: 'Blog',
+   // title: {
+   //    absolute: 'Blog', // overrides 'template'
+   // },
+};
+
 export default function Blog() {
    return <div>Blog</div>;
 }
